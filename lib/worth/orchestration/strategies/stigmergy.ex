fd6f9d9a06a7cd31ddcf26d10941803b6a9756b6
Lenz Gschwendtner <mail@norbu09.org>
2026-04-14 13:02:46 +0100

    fix: remove @impl true from handle_event/2 to match updated AgentEx.Strategy behaviour

--- lib/worth/orchestration/strategies/stigmergy.ex
+++ lib/worth/orchestration/strategies/stigmergy.ex
@@ -72,7 +72,6 @@ defmodule Worth.Orchestration.Strategies.Stigmergy do
     {:done, {:error, reason}, state}
   end
 
-  @impl true
   def handle_event({:tool_use, name, _workspace_id}, state) when is_binary(name) do
     deposit_intention_pheromone(name, state.workspace)
     {:ok, state}
